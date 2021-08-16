b6761011f4179b274a1f9ba52c3def4803e0a1bf
frontjim <frontjim@users.noreply.github.com>
2021-08-16 23:18:08 +0300

    EGINE TO SET UP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      trafa hamisou bro
-      
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>par tarxidia m</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ msg || 'Pou sai re arxidi' }}</h1>
+    <div class="wrapper">Add An Item :<br>
+      <div class="additem">
+        <label>
+          <input v-model="newitem" type="text" class="form__input" id="name" placeholder="Add Item..."/>
+        </label>
+        <button v-on:click="items.push({id:items.lenght +1, label: newitem})">Add</button>
+      </div>
+     <br>
+     <ul>
+       <li v-for="item in items" :key="item.id">{{item.label}}</li>
+     </ul>
+    </div>
   </div>
 </template>
 
@@ -35,12 +21,42 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+   data() {
+    return {
+      newitem :'',
+      items:[
+
+      ]
   }
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.form__input {
+  font-family: 'Roboto', sans-serif;
+  color: #333;
+  font-size: 1.2rem;
+	margin: 0 auto;
+  padding: 1.5rem 2rem;
+  border-radius: 0.2rem;
+  background-color: rgb(255, 255, 255);
+  border: none;
+  width: 50%;
+  display: block;
+  border-bottom: 0.3rem solid transparent;
+  transition: all 0.3s;
+
+}
+.wrapper {
+  margin : auto;
+  width : 40vw;
+  height: 20vh;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.4);
+}
 h3 {
   margin: 40px 0 0;
 }
